928b525436424c4e3bb1fa5faf1f791727409c75
Matt Casanova <matt@codejo.io>
2022-06-17 21:05:22 -0700

    Laracasts: LaravelFromScratch8 ep 7 completed

---
+++ resources/views/post.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+    <article>
+        <a href="/post"><h1>My First Post</h1></a>
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Imperdiet nulla malesuada pellentesque elit eget gravida cum sociis. Dolor sit amet consectetur adipiscing elit ut aliquam purus. Lacus sed viverra tellus in hac habitasse platea. Elit scelerisque mauris pellentesque pulvinar pellentesque. Nibh ipsum consequat nisl vel pretium lectus quam id leo. Ultrices sagittis orci a scelerisque purus semper eget duis at. Egestas tellus rutrum tellus pellentesque eu tincidunt tortor aliquam. Id velit ut tortor pretium viverra suspendisse potenti. Leo integer malesuada nunc vel risus commodo viverra maecenas accumsan.
+        </p>
+    </article>
+
+    <a href="/">Go Back</a>
+</body>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+    <article>
+        <a href="/post"><h1>My First Post</h1></a>
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Imperdiet nulla malesuada pellentesque elit eget gravida cum sociis. Dolor sit amet consectetur adipiscing elit ut aliquam purus. Lacus sed viverra tellus in hac habitasse platea. Elit scelerisque mauris pellentesque pulvinar pellentesque. Nibh ipsum consequat nisl vel pretium lectus quam id leo. Ultrices sagittis orci a scelerisque purus semper eget duis at. Egestas tellus rutrum tellus pellentesque eu tincidunt tortor aliquam. Id velit ut tortor pretium viverra suspendisse potenti. Leo integer malesuada nunc vel risus commodo viverra maecenas accumsan.
+        </p>
+    </article>
+
+    <article>
+        <a href="/post"><h1>My Second Post</h1></a>
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Imperdiet nulla malesuada pellentesque elit eget gravida cum sociis. Dolor sit amet consectetur adipiscing elit ut aliquam purus. Lacus sed viverra tellus in hac habitasse platea. Elit scelerisque mauris pellentesque pulvinar pellentesque. Nibh ipsum consequat nisl vel pretium lectus quam id leo. Ultrices sagittis orci a scelerisque purus semper eget duis at. Egestas tellus rutrum tellus pellentesque eu tincidunt tortor aliquam. Id velit ut tortor pretium viverra suspendisse potenti. Leo integer malesuada nunc vel risus commodo viverra maecenas accumsan.
+        </p>
+    </article>
+
+    <article>
+        <a href="/post"><h1>My Third Post</h1></a>
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Imperdiet nulla malesuada pellentesque elit eget gravida cum sociis. Dolor sit amet consectetur adipiscing elit ut aliquam purus. Lacus sed viverra tellus in hac habitasse platea. Elit scelerisque mauris pellentesque pulvinar pellentesque. Nibh ipsum consequat nisl vel pretium lectus quam id leo. Ultrices sagittis orci a scelerisque purus semper eget duis at. Egestas tellus rutrum tellus pellentesque eu tincidunt tortor aliquam. Id velit ut tortor pretium viverra suspendisse potenti. Leo integer malesuada nunc vel risus commodo viverra maecenas accumsan.
+        </p>
+    </article>
+
+</body>
