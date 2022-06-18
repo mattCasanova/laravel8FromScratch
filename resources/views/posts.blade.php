64e754149368852d1882705d86c714ef6123d993
Matt Casanova <matt@codejo.io>
2022-06-18 12:47:14 -0700

    Laracasts completed another ep

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,28 +4,10 @@
 <link rel="stylesheet" href="/app.css">
 
 <body>
+    <?php foreach ($posts as $post) : ?>
     <article>
-        <a href="/post/my-first-post"><h1>My First Post</h1></a>
-
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Imperdiet nulla malesuada pellentesque elit eget gravida cum sociis. Dolor sit amet consectetur adipiscing elit ut aliquam purus. Lacus sed viverra tellus in hac habitasse platea. Elit scelerisque mauris pellentesque pulvinar pellentesque. Nibh ipsum consequat nisl vel pretium lectus quam id leo. Ultrices sagittis orci a scelerisque purus semper eget duis at. Egestas tellus rutrum tellus pellentesque eu tincidunt tortor aliquam. Id velit ut tortor pretium viverra suspendisse potenti. Leo integer malesuada nunc vel risus commodo viverra maecenas accumsan.
-        </p>
+        <?= $post ?>
     </article>
-
-    <article>
-        <a href="/post/my-second-post"><h1>My Second Post</h1></a>
-
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Imperdiet nulla malesuada pellentesque elit eget gravida cum sociis. Dolor sit amet consectetur adipiscing elit ut aliquam purus. Lacus sed viverra tellus in hac habitasse platea. Elit scelerisque mauris pellentesque pulvinar pellentesque. Nibh ipsum consequat nisl vel pretium lectus quam id leo. Ultrices sagittis orci a scelerisque purus semper eget duis at. Egestas tellus rutrum tellus pellentesque eu tincidunt tortor aliquam. Id velit ut tortor pretium viverra suspendisse potenti. Leo integer malesuada nunc vel risus commodo viverra maecenas accumsan.
-        </p>
-    </article>
-
-    <article>
-        <a href="/post/my-third-post"><h1>My Third Post</h1></a>
-
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Imperdiet nulla malesuada pellentesque elit eget gravida cum sociis. Dolor sit amet consectetur adipiscing elit ut aliquam purus. Lacus sed viverra tellus in hac habitasse platea. Elit scelerisque mauris pellentesque pulvinar pellentesque. Nibh ipsum consequat nisl vel pretium lectus quam id leo. Ultrices sagittis orci a scelerisque purus semper eget duis at. Egestas tellus rutrum tellus pellentesque eu tincidunt tortor aliquam. Id velit ut tortor pretium viverra suspendisse potenti. Leo integer malesuada nunc vel risus commodo viverra maecenas accumsan.
-        </p>
-    </article>
+    <?php endforeach; ?>
 
 </body>
