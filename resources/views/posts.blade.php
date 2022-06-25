c2825d576f08f20f92465e4e6266effd5138804b
Matt Casanova <matt@codejo.io>
2022-06-25 10:48:15 -0700

    Laravel8FromScratch completed ep 32

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,24 +2,18 @@
     @include('_posts-header')
 
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-        <x-post-featured-card />
+        @if ($posts->count())
+            <x-posts-grid :posts="$posts" />
+        @else
+        <p class="text-center">No posts yet.  Please check back later.</p>
+        @endif
 
-        <div class="lg:grid lg:grid-cols-2">
-            <x-post-card />
-            <x-post-card />
-        </div>
-
-        <div class="lg:grid lg:grid-cols-3">
-            <x-post-card />
-            <x-post-card />
-            <x-post-card />
-        </div>
     </main>
 </x-layout>
 
 {{-- Old posts page
     <x-slot name="header">
-        My Cool Blog
+    My Cool Blog
     </x-slot>
     <x-slot name="content">
     @foreach ($posts as $post)
