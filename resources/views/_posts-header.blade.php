e584fcfad23678ba0e83afa8adfdaefa52752739
Matt Casanova <matt@codejo.io>
2022-06-25 11:41:13 -0700

    Laravel8FromScratch completed ep 34

--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -3,7 +3,7 @@
         Latest <span class="text-blue-500">Laravel From Scratch</span> News
     </h1>
 
-    <h2 class="inline-flex mt-2">By Lary Laracore <img src="./images/lary-head.svg" alt="Head of Lary the mascot"></h2>
+    <h2 class="inline-flex mt-2">By Lary Laracore <img src="/images/lary-head.svg" alt="Head of Lary the mascot"></h2>
 
     <p class="text-sm mt-14">
         Another year. Another update. We're refreshing the popular Laravel series with new content.
@@ -25,8 +25,8 @@
                 <g fill="none" fill-rule="evenodd">
                     <path stroke="#000" stroke-opacity=".012" stroke-width=".5" d="M21 1v20.16H.84V1z">
                     </path>
-                    <path fill="#222"
-                        d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z"></path>
+                    <path fill="#222" d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z">
+                    </path>
                 </g>
             </svg>
         </div>
@@ -47,8 +47,8 @@
                 <g fill="none" fill-rule="evenodd">
                     <path stroke="#000" stroke-opacity=".012" stroke-width=".5" d="M21 1v20.16H.84V1z">
                     </path>
-                    <path fill="#222"
-                        d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z"></path>
+                    <path fill="#222" d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z">
+                    </path>
                 </g>
             </svg>
         </div>
